d6fa8f5ed357a55a733313d75a5a827b9a2db7ff
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-03-04 03:23:56 +0100

    chore(server): configure webpack dev server and implement react hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
     entry: './src/app.js',
     output: {
         filename: 'app.bundle.js',
@@ -14,7 +16,16 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                use: [
+                    {
+                        loader: require.resolve('babel-loader'),
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                            plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean)
+                        }
+
+                    }
+                ]
             },
             {
                 test: /\.s?css$/i,
@@ -25,6 +36,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/template.html'
-        })
-    ]
+        }),
+        isDevelopment && new ReactRefreshWebpackPlugin()
+    ].filter(Boolean),
+    devServer: {
+        static: './dist',
+        open: true,
+        hot: true
+    }
 };
