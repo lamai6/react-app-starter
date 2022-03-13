682beeac4a80db1e0f54b9914feaf53483d20ec2
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-03-13 12:17:25 +0100

    chore(linter): add airbnb eslint rules to the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:jest/recommended"
+    extends: [
+        'airbnb',
+        'plugin:react/jsx-runtime',
+        'plugin:jest/recommended'
     ],
-    "parser": "@babel/eslint-parser",
-    "plugins": ["jest"],
-    "settings": {
-        "react": {
-            "version": "detect"
+    parser: '@babel/eslint-parser',
+    plugins: ['jest'],
+    settings: {
+        'react': {
+            'version': 'detect'
         },
-        "jest": {
-            "version": require('jest/package.json').version,
+        'jest': {
+            'version': require('jest/package.json').version,
         }
     },
-    "env": {
-        "browser": true,
-        "node": true
-    }
+    env: {
+        'browser': true,
+        'node': true
+    },
+    ignorePatterns: ['node_modules', 'dist', 'coverage']
 }
