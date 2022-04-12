bd18bd5bec258c02f610325da3b585f4a8415885
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-12 18:51:34 +0200

    style(webpack): fix htmlWebpackPlugin indentation

--- webpack.config.js
+++ webpack.config.js
@@ -58,17 +58,17 @@ module.exports = {
       inject: false,
       favicon: './src/assets/images/favicon.png',
       templateContent: ({ htmlWebpackPlugin }) => `
-                <html>
-                    <head>
-                        <title>${htmlWebpackPlugin.options.title}</title>
-                        ${htmlWebpackPlugin.tags.headTags}
-                    </head>
-                    <body>
-                        <div id='app'></div>
-                        ${htmlWebpackPlugin.tags.bodyTags}
-                    </body>
-                </html>
-            `,
+        <html>
+            <head>
+                <title>${htmlWebpackPlugin.options.title}</title>
+                ${htmlWebpackPlugin.tags.headTags}
+            </head>
+            <body>
+                <div id='app'></div>
+                ${htmlWebpackPlugin.tags.bodyTags}
+            </body>
+        </html>
+      `,
     }),
   ].filter(Boolean),
   devServer: {
